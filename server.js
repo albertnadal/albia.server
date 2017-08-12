09273d362d8615ace1587d2103d76590e3bdea11
Albert Nadal <a.nadal@iskra.cat>
2017-08-12 18:41:25 +0200

    Added serialization and deserialization sample code using protobuf.

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var messages = require('./albia_pb');
 var message = new messages.DeviceRecord();
 
+var currentTimestampUTC = new messages.google.protobuf.Timestamp();
+currentTimestampUTC.setSeconds(Math.floor((new Date()).getTime() / 1000)); // UNIX Timestamp in UTC
+
+message.setKey('year');
+message.setDate(currentTimestampUTC);
+message.setType(messages.DeviceRecord.RecordType.INT32);
+message.setInt32value(1981);
+
+var serializedMessage = message.serializeBinary();
+var messageReceived = messages.DeviceRecord.deserializeBinary(serializedMessage);
+console.log("KEY: "+messageReceived.getKey()+" VALUE: "+messageReceived.getInt32value());
+
 var SocketIONamespacesManager = require("./SocketIONamespacesManager.js");
 var APIServer = require("./APIServer.js");
 var WebSocketServer = require("./WebSocketServer.js");
